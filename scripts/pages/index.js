08a2827b937d444bf835792627461185338387de
Elishaya13 <a.lartillier@gmail.com>
2023-10-09 14:47:22 +0200

    First commit - Project initialization
    - Added initial project structure
    - Included project images
    - Set up Sass for styling
    - Created the basic HTML structure
    - Added data file for project

---
+++ scripts/pages/index.js
@@ -0,0 +1,11 @@
+class Index {
+  constructor() {
+    this.$headerWrapper = document.getElementById('header_wrapper');
+    this.$mainContent = document.getElementById('main_content');
+  }
+  main() {
+    console.log('coucou');
+  }
+}
+const index = new Index();
+index.main();
