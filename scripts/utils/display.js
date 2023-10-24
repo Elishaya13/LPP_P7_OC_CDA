2fb187c60565cbd3b035d3e5427b024643d8ad58
Elishaya13 <a.lartillier@gmail.com>
2023-10-24 16:15:49 +0200

    feat(search) - Resolve issue with file naming : display.js

--- scripts/utils/display.js
+++ scripts/utils/display.js
@@ -2,7 +2,6 @@ import { RecipeCard } from '../component/RecipeCard.js';
 import { FiltersWrapper } from '../templates/FiltersWrapper.js';
 
 export function displayRecipes(domElement, recipesList) {
-  console.log('construit la vue des recettes');
   domElement = domElement ?? document.querySelector('.recipes__wrapper');
 
   domElement.innerHTML = '';
@@ -16,15 +15,12 @@ export function displayRecipes(domElement, recipesList) {
 }
 
 export function displayCounter(recipesFiltered) {
-  //recupere l'element du Dom
   const $counter = document.querySelector('.filter__counter__span');
 
   // 's' or not
   recipesFiltered.length > 1
     ? ($counter.textContent = `${recipesFiltered.length} recettes `)
     : ($counter.textContent = `${recipesFiltered.length} recette `);
-
-  //console.log('met a jour le compteur de recette');
 }
 
 export function displayMenuFilter(domElement, recipesList, search) {
@@ -33,6 +29,4 @@ export function displayMenuFilter(domElement, recipesList, search) {
   const filters = new FiltersWrapper(domElement, recipesList, search);
 
   filters.createFiltersWrapper();
-
-  //console.log('met a jour le contenu des menu');
 }
